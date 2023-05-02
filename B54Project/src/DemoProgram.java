bb0fdb96e53eb4df53639f41cb6691e159a3aa4c
HP <HP@LAPTOP-7C3HG0VV>
2023-05-02 17:04:18 +0530

    First commit

---
+++ B54Project/src/DemoProgram.java
@@ -0,0 +1,11 @@
+
+//This program is to demonstatrate first progrma of java
+//Program to demonstrate addition of two numbers
+public class DemoProgram {
+
+	public static void main(String[] args) {
+		System.out.println();
+
+	}
+
+}
